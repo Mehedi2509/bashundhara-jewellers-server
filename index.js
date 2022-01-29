d7f05ce94c55ede9ca8cde9da4cd21440fbec0ee
Mahadi2509 <mahadi2509@gmail.com>
2022-01-29 22:53:32 +0600

    complete review,report database server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
+const { application } = require('express');
 require('dotenv').config();
 
 const app = express();
@@ -12,20 +13,97 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdula.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log(uri)
 
 const run = async () => {
     try {
         await client.connect();
         const database = client.db('bashundhara_jewellers');
-        const productsCollection = database.collection('products')
+        const productsCollection = database.collection('products');
+        const usersCollection = database.collection('users');
+        const reportsCollection = database.collection('reports');
+        const reviewsCollection = database.collection('reviews');
 
+        // product database
         app.get('/products', async (req, res) => {
+            // const limit = req.query.limit;
             const query = {};
             const cursor = productsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result);
         })
+
+        // users database
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const requester = req.decodeEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make an admin' })
+            }
+        });
+
+        // reports database
+        app.post('/reports', async (req, res) => {
+            const report = req.body;
+            const result = await reportsCollection.insertOne(report);
+            res.json(result);
+        });
+
+        // reviews database
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post('/reviews', async (req, res) => {
+            const reviewData = req.body;
+            const result = await reviewsCollection.insertOne(reviewData);
+            res.json(result);
+        });
 
     }
     finally {
